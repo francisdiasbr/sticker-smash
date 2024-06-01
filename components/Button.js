2d3c54c57391a1213f43005e28f2fd9ddc2ed0c5
Francis Dias <francisdiasbr@gmail.com>
2024-06-01 18:54:27 -0300

    Action button component to select a sticker, reset and save

--- components/Button.js
+++ components/Button.js
@@ -22,7 +22,7 @@ export default function Button({ label, onPress, theme }) {
   }
   return (
     <View style={styles.buttonContainer}>
-      <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+      <Pressable style={styles.button} onPress={onPress}>
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
     </View>
